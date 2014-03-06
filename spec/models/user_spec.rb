9498f0c06a6c65ece70a8a0bc55f82d9602a4b5d
megacartridge <mdltrack@gmail.com>
2014-03-05 19:06:02 -0500

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,12 @@ describe User do
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -159,6 +165,7 @@ describe "micropost associations" do
     let!(:newer_micropost) do
         FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
     end
+    let(:followed_user)
     
 describe "status" do
     let(:unfollowed_post) do
@@ -168,6 +175,34 @@ describe "status" do
     its(:feed) { should include(newer_micropost) }
     its(:feed) { should include(older_micropost) }
     its(:feed) { should_not include(unfollowed_post) }
+    its(:feed) do
+        followed_user.microposts.each do |micropost|
+            should include(micropost)
+        end
+    end
+end
+
+describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+        @user.save
+        @user.follow!(other_user)
+    end
+    
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+    
+    describe "followed user" do
+        subject { other_user }
+        its(:followers) { should include(@user) }
+    end
+end
+
+describe "and unfollowing" do
+    before { @user.unfollow!(other_user) }
+    
+    it { should_not be_following(other_user) }
+    its(:followed_users) { should_not include(other_user) }
 end
 end
 end
